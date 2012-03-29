15016f9054afb9dc7272788cc57c5fd366818682
mogilny <mogilny@Robert-laptop>
2012-03-29 07:31:48 -0400

    first commit

---
+++ SirisScraper/src/ca/androidclub/app/SirisScraperActivity.java
@@ -0,0 +1,21 @@
+package ca.androidclub.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SirisScraperActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+    
+    public void onClickSubmit(View v)
+    {
+    	
+    }
+    
+    
+}
